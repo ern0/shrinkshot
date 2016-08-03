94eb33137003a3f29d0c8c7e50b584e2845f5038
ern0 <ern0@linkbroker.hu>
2016-08-03 21:26:51 +0200

    procSide() started

--- s
+++ s
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./shrinkshot test/c*
+./shrinkshot test/d*
--- shrinkshot.cpp
+++ shrinkshot.cpp
@@ -2,95 +2,120 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "upng/upng.h"
+# define RET
+
+class ShrinkShot {
+
+	private:
+	
+	int width;
+	int height;
+	const unsigned char* buffer;
+	int bytesPerPix;
+
+	int* diffCount;
+	int* diffValue;
+
+	int hShrinkPos;
+	int hShrinkLength;
+	int vShrinkPos;
+	int vShrinkLength;
 
 
-int width;
-int height;
-const unsigned char* buffer;
-int bytesPerPix;
+	private: void sig() {
+		fprintf(stderr,"shrinkshot: ");
+	} // sig()
 
 
-int pix(int y,int x) {
+	public:	int main(int argc,char* argv[]) {
 
-	unsigned int offset = bytesPerPix * ( (y * width) + x );
-	unsigned int value = buffer[offset];
-	value += buffer[1 + offset];
-	value += buffer[2 + offset];
-	if (bytesPerPix == 4) {
-		value += buffer[3 + offset];
-	} else {
-		value += buffer[1 + offset];
-	}
-	value = value >> 2;
+		if (argc != 2) {
+			fprintf(stderr,"specify filename \n");
+			exit(0);
+		}
 
-	return value;
-} // pix()
+		upng_t* upng;
 
+		upng = upng_new_from_file(argv[1]);
+		if (upng != NULL) {
+			upng_decode(upng);
+			if (upng_get_error(upng) == UPNG_EOK) {
 
-void proc(upng_t* upng) {
+				width = upng_get_width(upng);
+				height = upng_get_height(upng);
+				buffer = upng_get_buffer(upng);
+				int bpp = upng_get_bpp(upng);
 
-	fprintf(stderr,"%d x %d :: %d \n",width,height,bytesPerPix);
+				bool okay = false;
+				if (bpp == 24) okay = true;
+				if (bpp == 32) okay = true;
+				if (!okay) {
+					sig();
+					fprintf(stderr,"unsupported bit-per-pixel: %d \n",bpp);
+					exit(1);
+				}
 
-	for (int n = 0; n < 33; n++) {
-		printf("%d: %d \n",n,pix(n,n));
-	}
+				bytesPerPix = bpp >> 3;
+				proc(upng);
+			
+			} // if okay
 
-} // proc()
-
-
-void sig() {
-	fprintf(stderr,"shrinkshot: ");
-} // sig()
-
-
-int main(int argc,char* argv[]) {
-
-	if (argc != 2) {
-		fprintf(stderr,"specify filename \n");
-		exit(0);
-	}
-
-	upng_t* upng;
-
-	upng = upng_new_from_file(argv[1]);
-	if (upng != NULL) {
-		upng_decode(upng);
-		if (upng_get_error(upng) == UPNG_EOK) {
-
-			width = upng_get_width(upng);
-			height = upng_get_height(upng);
-			buffer = upng_get_buffer(upng);
-			int bpp = upng_get_bpp(upng);
-
-			bool okay = false;
-			if (bpp == 24) okay = true;
-			if (bpp == 32) okay = true;
-			if (!okay) {
+			else {
 				sig();
-				fprintf(stderr,"unsupported bit-per-pixel: %d \n",bpp);
+				fprintf(stderr,"error processing file %s \n",argv[1]);
 				exit(1);
 			}
 
-			bytesPerPix = bpp >> 3;
-			proc(upng);
-		
-		} // if okay
+			upng_free(upng);
+
+		} // if upng from file
 
 		else {
 			sig();
-			fprintf(stderr,"error processing file %s \n",argv[1]);
+			fprintf(stderr,"error loading file %s \n",argv[1]);
 			exit(1);
 		}
 
-		upng_free(upng);
+		return 0;
+	} // main()
 
-	} // if upng from file
 
-	else {
-		sig();
-		fprintf(stderr,"error loading file %s \n",argv[1]);
-		exit(1);
-	}
+	private: int gray(int y,int x) {
 
-	return 0;
+		unsigned int offset = bytesPerPix * ( (y * width) + x );
+		unsigned int value = buffer[offset];
+		value += buffer[1 + offset];
+		value += buffer[2 + offset];
+		if (bytesPerPix == 4) {
+			value += buffer[3 + offset];
+		} else {
+			value += buffer[1 + offset];
+		}
+		value = value >> 2;
+
+		return value;
+	} // gray()
+
+
+	private: void proc(upng_t* upng) {
+
+		fprintf(stderr,"%d x %d \n",width,height);
+
+		procSide(RET &hShrinkPos,RET &hShrinkLength,'h',width,height);
+		procSide(RET &vShrinkPos,RET &vShrinkLength,'v',height,width);
+
+	} // proc()
+
+
+	private: void procSide(RET int* pos,RET int* length,char mod,int pri,int sec) {
+
+	} // procSide()
+
+
+}; // class ShrinkShot
+
+
+int main(int argc,char* argv[]) {
+	ShrinkShot shrinkshot;
+	return shrinkshot.main(argc,argv);
 } // main()
